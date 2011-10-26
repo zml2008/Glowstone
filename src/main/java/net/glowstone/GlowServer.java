15e1cc64563c1aab273b44cd3538a9c2963f99b7
zml2008 <zach@zachsthings.com>
2011-10-26 07:48:00 -0700

    Most of the Material deps removed, except where needed by API.

--- src/main/java/net/glowstone/GlowServer.java
+++ src/main/java/net/glowstone/GlowServer.java
@@ -203,6 +203,11 @@ public final class GlowServer implements Server {
     private GameMode defaultGameMode = GameMode.SURVIVAL;
 
     /**
+     * Whether the server is shutting down
+     */
+    private boolean isShuttingDown = false;
+
+    /**
      * Creates a new server.
      */
     public GlowServer() {
@@ -449,6 +454,9 @@ public final class GlowServer implements Server {
      * Stops this server.
      */
     public void shutdown() {
+        // This is so we don't run this twice (/stop and actual shutdown)
+        if (isShuttingDown) return;
+        isShuttingDown = true;
         logger.info("The server is shutting down...");
         
         // Stop scheduler and disable plugins
